e82ba4349fce843df92684a9fe04321f3a8c75eb
Hamilton Karl <hamilton@nonfatmedia.com>
2015-08-21 14:45:14 -0700

    modified animal package and created to show adjective plus animal

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Random Animal" });
-});
-
-router.get('/animal', function(req, res, next){
 	var animals = require('animals');
 	var animal = animals();
+  res.render('index', { title: animal });
 });
 
 module.exports = router;
